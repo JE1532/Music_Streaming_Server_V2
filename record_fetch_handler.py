c3f21a5a33e637ee7cc50d3acd3525780451d9a0
Jonathan Elfassy <jonatanalf2016@gmail.com>
2023-05-01 13:08:33 +0300

    added tracklists

--- record_fetch_handler.py
+++ record_fetch_handler.py
@@ -8,22 +8,29 @@ RESPONSE_PREFIX = b'Gui/Fetched/'
 FORMAT_LIKES_AND_LISTENNINGS = lambda name, likes, listennings: f'name={name}&likes={likes}&listennings={listennings}&picture='.encode()
 SONG_DATABASE = 'songs.db'
 FETCH_RECORD_LIKES_AND_LISTENNINGS = lambda name: f'SELECT likes, listennings FROM records WHERE name="{name}"'
+IS_PLAYLIST_TRACKLIST = '^tracks:'
+PLAYLIST_RESPONSE_PREFIX = 'Gui/Fetched/'.encode()
+GET_TRACKLIST_PATH = lambda playlist: f'music/{playlist}/tracklist.txt'
 
 
 def record_fetch_handler(fetch_queue, send_queue):
     conn = sqlite3.connect(SONG_DATABASE)
     crsr = conn.cursor()
-    crsr.execute('DROP TABLE records;')
-    crsr.execute('CREATE TABLE records(name VARCHAR(64), likes INT, listennings INT, PRIMARY KEY(name));')
-    crsr.execute('INSERT INTO records VALUES("longSoundExample", 50, 79);')
-    crsr.execute('INSERT INTO records VALUES("song2", 57, 64);')
-    crsr.execute('INSERT INTO records VALUES("song3", 47, 34);')
+    #crsr.execute('DROP TABLE records;')
+    #crsr.execute('CREATE TABLE records(name VARCHAR(64), likes INT, listennings INT, PRIMARY KEY(name));')
+    #crsr.execute('INSERT INTO records VALUES("longSoundExample", 50, 79);')
+    #crsr.execute('INSERT INTO records VALUES("song2", 57, 64);')
+    #crsr.execute('INSERT INTO records VALUES("song3", 47, 34);')
+    #conn.commit()
     while True:
         fetch_request, cli_sock = fetch_queue.get()
         try:
             prefix, record_name = fetch_request.split('/')
         except:
             raise Exception('Invalid Fetch Request:', fetch_request)
+        if len(record_name) >= len(IS_PLAYLIST_TRACKLIST) and record_name[:len(IS_PLAYLIST_TRACKLIST)] == IS_PLAYLIST_TRACKLIST:
+            send_queue.put((fetch_tracklist(record_name), cli_sock))
+            continue
         try:
             likes, listennings = crsr.execute(FETCH_RECORD_LIKES_AND_LISTENNINGS(record_name)).fetchall()[0]
         except:
@@ -33,3 +40,10 @@ def record_fetch_handler(fetch_queue, send_queue):
             pic_data = pic_file.read()
             response.extend(pic_data)
         send_queue.put((bytes(response), cli_sock))
+
+
+def fetch_tracklist(record_name):
+    playlist = record_name[len(IS_PLAYLIST_TRACKLIST):]
+    with open(GET_TRACKLIST_PATH(playlist), 'rb') as f:
+        response = PLAYLIST_RESPONSE_PREFIX + f.read()
+    return response
--- search_fetch_handler.py
+++ search_fetch_handler.py
@@ -1,7 +1,7 @@
 GET_SONG_RECOMMENATIONS = 'Search?prompt=^Song_Recommendations'
-GET_ALBUM_RECOMMENDATIONS = 'Search?prompt=^Song_Recommendations'
+GET_ALBUM_RECOMMENDATIONS = 'Search?prompt=^Playlist_Recommendations'
 SONG_RECOMMENDATION_PATH = 'music/song_recommendations.txt'
-ALBUM_RECOMMENDATIONS_PATH = 'music/album_recommendations.txt'
+ALBUM_RECOMMENDATIONS_PATH = 'music/playlist_recommendations.txt'
 RESPONSE_PREFIX = 'Gui/Searched/'
 
 def search_fetch(search_queue, send_queue):
@@ -10,4 +10,7 @@ def search_fetch(search_queue, send_queue):
         print(f'searched for: {to_search}')
         if to_search == GET_SONG_RECOMMENATIONS:
             with open(SONG_RECOMMENDATION_PATH, 'rb') as f:
+                send_queue.put((RESPONSE_PREFIX.encode() + f.read(), cli_sock))
+        elif to_search == GET_ALBUM_RECOMMENDATIONS:
+            with open(ALBUM_RECOMMENDATIONS_PATH, 'rb') as f:
                 send_queue.put((RESPONSE_PREFIX.encode() + f.read(), cli_sock))
